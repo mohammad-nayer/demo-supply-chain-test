e8a79f873b8109fe2f476da27b1cff1c514949ff
mohammad-nayer <32834715+mohammad-nayer@users.noreply.github.com>
2025-06-25 14:44:20 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+# This is a python script
+
+bebop = "Bebop the Robot"
+haze = "Haze the Assassin"
+
+def talk(name):
+    if name == "Bebop the Robot":
+        print("Beep boop robot noises")
+    elif name == "Bebop the Robot":
+        print("I am the shadows")
+
+
+talk(haze)
